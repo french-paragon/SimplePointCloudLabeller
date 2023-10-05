03580f81b61ceec59c103db61a05e492319206f4
french-paragon <french.paragon@gmail.com>
2023-10-05 16:43:47 +0200

    Improve the point cloud reading function
    Now we read the file with all the fields.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -93,20 +93,271 @@ void MainWindow::setPossibleLabels(QStringList const& labels) {
 
 }
 
+template<uint8_t type>
+float getFieldValue(void* ptr) {
+
+    switch (type) {
+    case pcl::PCLPointField::INT8 :
+    {
+        int8_t rawVal = *reinterpret_cast<int8_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::UINT8 :
+    {
+        uint8_t rawVal = *reinterpret_cast<uint8_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::INT16 :
+    {
+        int16_t rawVal = *reinterpret_cast<int16_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::UINT16 :
+    {
+        uint16_t rawVal = *reinterpret_cast<uint16_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::INT32 :
+    {
+        int32_t rawVal = *reinterpret_cast<int32_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::UINT32 :
+    {
+        uint32_t rawVal = *reinterpret_cast<uint32_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::FLOAT32 :
+    {
+        float val = *reinterpret_cast<float*>(ptr);
+        return val;
+    }
+    case pcl::PCLPointField::FLOAT64 :
+    {
+        double rawVal = *reinterpret_cast<double*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    default:
+        break;
+    }
+
+    return 0.0;
+}
+
+float getFieldValue(void* ptr, uint8_t type) {
+
+    switch (type) {
+    case pcl::PCLPointField::INT8 :
+    {
+        int8_t rawVal = *reinterpret_cast<int8_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::UINT8 :
+    {
+        uint8_t rawVal = *reinterpret_cast<uint8_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::INT16 :
+    {
+        int16_t rawVal = *reinterpret_cast<int16_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::UINT16 :
+    {
+        uint16_t rawVal = *reinterpret_cast<uint16_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::INT32 :
+    {
+        int32_t rawVal = *reinterpret_cast<int32_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::UINT32 :
+    {
+        uint32_t rawVal = *reinterpret_cast<uint32_t*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    case pcl::PCLPointField::FLOAT32 :
+    {
+        float val = *reinterpret_cast<float*>(ptr);
+        return val;
+    }
+    case pcl::PCLPointField::FLOAT64 :
+    {
+        double rawVal = *reinterpret_cast<double*>(ptr);
+        return static_cast<float>(rawVal);
+    }
+    default:
+        break;
+    }
+
+    return 0.0;
+}
+
+float colorScaleFromType(uint8_t type) {
+    switch (type) {
+    case pcl::PCLPointField::INT8 :
+        return 2;
+    case pcl::PCLPointField::UINT8 :
+        return 1;
+    case pcl::PCLPointField::INT16 :
+        return 1./(256);
+    case pcl::PCLPointField::UINT16 :
+        return 1./(256);
+    case pcl::PCLPointField::INT32 :
+        return 1./(256*256*256);
+    case pcl::PCLPointField::UINT32 :
+        return 1./(256*256*256);
+    case pcl::PCLPointField::FLOAT32 :
+        return 255;
+    case pcl::PCLPointField::FLOAT64 :
+        return 255;
+    default:
+        break;
+    }
+
+    return 1.0;
+}
+
 bool MainWindow::openPointCloud(QString const& path) {
 
-    pcl::PointCloud<pcl::PointXYZRGB> cloud;
+    pcl::PCLPointCloud2 cloud;
 
-    if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (path.toStdString(), cloud) == -1) {
+    if (pcl::io::loadPCDFile (path.toStdString(), cloud) == -1) {
         return false;
     }
 
-    QVector<QVector3D> points(cloud.size());
-    QVector<QColor> colors(cloud.size());
+    int pointStructSize = cloud.point_step; //size of a point struct
 
-    for (int i = 0; i < cloud.size(); i++) {
-        points[i] = QVector3D(cloud[i].x, cloud[i].y, cloud[i].z);
-        colors[i] = QColor(cloud[i].r, cloud[i].g, cloud[i].b);
+    //fields offsets
+    int x_field_offset = -1;
+    int y_field_offset = -1;
+    int z_field_offset = -1;
+
+    int r_field_offset = -1;
+    int g_field_offset = -1;
+    int b_field_offset = -1;
+
+    int rgb_field_offset = -1;
+
+    //fields types
+    uint8_t x_field_type = 0;
+    uint8_t y_field_type = 0;
+    uint8_t z_field_type = 0;
+
+    uint8_t r_field_type = 0;
+    uint8_t g_field_type = 0;
+    uint8_t b_field_type = 0;
+
+    for (pcl::PCLPointField const& field : cloud.fields) {
+
+        if (QString::fromStdString(field.name).toLower() == "x") {
+            x_field_offset = field.offset;
+            x_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "y") {
+            y_field_offset = field.offset;
+            y_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "z") {
+            z_field_offset = field.offset;
+            z_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "r") {
+            r_field_offset = field.offset;
+            r_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "g") {
+            g_field_offset = field.offset;
+            g_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "b") {
+            b_field_offset = field.offset;
+            b_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "red") {
+            r_field_offset = field.offset;
+            r_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "green") {
+            g_field_offset = field.offset;
+            g_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "blue") {
+            b_field_offset = field.offset;
+            b_field_type = field.datatype;
+        }
+
+        if (QString::fromStdString(field.name).toLower() == "rgb" and
+                field.datatype == pcl::PCLPointField::UINT32) {
+            rgb_field_offset = field.offset;
+        }
+
+    }
+
+    if (x_field_offset < 0 or y_field_offset < 0 or z_field_offset < 0) {
+        return false;
+    }
+
+    //get the r, g and b fields from rgb if no individual fields have been found
+    if (rgb_field_offset >= 0 and
+            (r_field_offset < 0 or g_field_offset < 0 or b_field_offset < 0)) {
+
+        b_field_offset = rgb_field_offset;
+        g_field_offset = rgb_field_offset+1;
+        r_field_offset = rgb_field_offset+2;
+
+        r_field_type = pcl::PCLPointField::UINT8;
+        g_field_type = pcl::PCLPointField::UINT8;
+        b_field_type = pcl::PCLPointField::UINT8;
+    }
+
+    float r_scale = colorScaleFromType(r_field_type);
+    float g_scale = colorScaleFromType(g_field_type);
+    float b_scale = colorScaleFromType(b_field_type);
+
+    auto cloudSize = cloud.height*cloud.width;
+
+    QVector<QVector3D> points(cloudSize);
+    QVector<QColor> colors(cloudSize);
+
+    for (int i = 0; i < cloudSize; i++) {
+
+        uint8_t* dataRow = cloud.data.data() + i*pointStructSize;
+
+        float x = getFieldValue(dataRow + x_field_offset, x_field_type);
+        float y = getFieldValue(dataRow + y_field_offset, y_field_type);
+        float z = getFieldValue(dataRow + z_field_offset, z_field_type);
+
+        float r = 128;
+        float g = 128;
+        float b = 128;
+
+        if (r_field_offset > 0) {
+            r = getFieldValue(dataRow + r_field_offset, r_field_type);
+            r *= r_scale;
+        }
+
+        if (g_field_offset > 0) {
+            g = getFieldValue(dataRow + g_field_offset, g_field_type);
+            g *= g_scale;
+        }
+
+        if (b_field_offset > 0) {
+            b = getFieldValue(dataRow + b_field_offset, b_field_type);
+            b *= b_scale;
+        }
+
+        points[i] = QVector3D(x, y, z);
+        colors[i] = QColor(static_cast<int>(r), static_cast<int>(g), static_cast<int>(b));
     }
 
 
